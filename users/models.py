68688156eddfdf972f62047272d12911f2bed4a1
Виолетта <viol.gerasimova2015@yandex.ru>
2025-08-19 18:22:35 +0400

    added base user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
 
 # Create your models here.
