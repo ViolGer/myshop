6614c041e7debd7ab8b0250d7154ee371ac98341
Виолетта <viol.gerasimova2015@yandex.ru>
2025-08-20 16:15:32 +0400

    added a few fields to user, registration.html and user registration form

--- config/settings.py
+++ config/settings.py
@@ -16,9 +16,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'products',
-    'users',
 ]
 
 MIDDLEWARE = [
--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.form import UserRegistrationForm
+
+
+class UserCreationView(CreateView):
+    form_class = UserRegistrationForm
+    template_name = 'users/register.html'
+    success_url = '/'
+
+
