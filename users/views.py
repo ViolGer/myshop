81cda9bbaeefad7bceded4ce61295b837d9f0343
Виолетта <viol.gerasimova2015@yandex.ru>
2025-08-25 16:18:50 +0400

    added mock account page

--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
-from django.views.generic import CreateView, FormView
+from django.views.generic import CreateView, FormView, TemplateView
 
-from users.form import UserRegistrationForm, UserLoginForm
+from .form import UserRegistrationForm
 
 
 class UserCreationView(CreateView):
     form_class = UserRegistrationForm
     template_name = 'users/register.html'
     success_url = '/'
+
+class AccountView(TemplateView):
+    template_name = 'users/account.html'
